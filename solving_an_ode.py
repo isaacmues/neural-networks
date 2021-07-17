de7aef61201e0e457954fb2dd5297a8e703cd58c
Isaac Muñoz <isaacmues@gmail.com>
2021-07-17 17:56:27 -0500

    Reformat with black

--- solving_an_ode.py
+++ solving_an_ode.py
@@ -12,7 +12,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import os
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 # initial condition
 f0 = 1
@@ -23,24 +23,24 @@ inf_s = np.sqrt(np.finfo(np.float32).eps)
 learning_rate = 0.01
 training_steps = 1000
 batch_size = 100
-display_step = training_steps/10
+display_step = training_steps / 10
 
 # Network Parameters
-n_input = 1     # input layer number of neurons
-n_hidden_1 = 32 # 1st layer number of neurons
-n_hidden_2 = 32 # 2nd layer number of neurons
-n_output = 1    # output layer number of neurons
+n_input = 1  # input layer number of neurons
+n_hidden_1 = 32  # 1st layer number of neurons
+n_hidden_2 = 32  # 2nd layer number of neurons
+n_output = 1  # output layer number of neurons
 
 weights = {
-'h1': tf.Variable(tf.random.normal([n_input, n_hidden_1])),
-'h2': tf.Variable(tf.random.normal([n_hidden_1, n_hidden_2])),
-'out': tf.Variable(tf.random.normal([n_hidden_2, n_output]))
+    "h1": tf.Variable(tf.random.normal([n_input, n_hidden_1])),
+    "h2": tf.Variable(tf.random.normal([n_hidden_1, n_hidden_2])),
+    "out": tf.Variable(tf.random.normal([n_hidden_2, n_output])),
 }
 
 biases = {
-'b1': tf.Variable(tf.random.normal([n_hidden_1])),
-'b2': tf.Variable(tf.random.normal([n_hidden_2])),
-'out': tf.Variable(tf.random.normal([n_output]))
+    "b1": tf.Variable(tf.random.normal([n_hidden_1])),
+    "b2": tf.Variable(tf.random.normal([n_hidden_2])),
+    "out": tf.Variable(tf.random.normal([n_output])),
 }
 
 # Stochastic gradient descent optimizer.
@@ -48,29 +48,34 @@ optimizer = tf.optimizers.SGD(learning_rate)
 
 # Create model
 def multilayer_perceptron(x):
-  x = np.array([[[x]]],  dtype='float32')
-  layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-  layer_1 = tf.nn.sigmoid(layer_1)
-  layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-  layer_2 = tf.nn.sigmoid(layer_2)
-  output = tf.matmul(layer_2, weights['out']) + biases['out']
-  return tf.nn.sigmoid(output)
+    x = np.array([[[x]]], dtype="float32")
+    layer_1 = tf.add(tf.matmul(x, weights["h1"]), biases["b1"])
+    layer_1 = tf.nn.sigmoid(layer_1)
+    layer_2 = tf.add(tf.matmul(layer_1, weights["h2"]), biases["b2"])
+    layer_2 = tf.nn.sigmoid(layer_2)
+    output = tf.matmul(layer_2, weights["out"]) + biases["out"]
+    return tf.nn.sigmoid(output)
+
 
 # Universal Approximator
 def g(x):
     return x * multilayer_perceptron(x) + f0
 
+
 # Given EDO
 def f(x):
-    return 2*x
-    
+    return 2 * x
+
+
 # Custom loss function to approximate the derivatives
 def custom_loss():
     def dNN(x):
-        return (g(x+inf_s) - g(x)) / inf_s
-    summation = [(dNN(x) - f(x))**2 for x in np.linspace(0, 1, 11)]
+        return (g(x + inf_s) - g(x)) / inf_s
+
+    summation = [(dNN(x) - f(x)) ** 2 for x in np.linspace(0, 1, 11)]
     return tf.reduce_sum(tf.abs(summation))
     # return tf.sqrt(tf.reduce_mean(tf.abs(summation)))
+
 
 def train_step():
     with tf.GradientTape() as tape:
@@ -79,6 +84,7 @@ def train_step():
     gradients = tape.gradient(loss, trainable_variables)
     optimizer.apply_gradients(zip(gradients, trainable_variables))
 
+
 for i in range(training_steps):
     train_step()
     if i % display_step == 0:
@@ -86,20 +92,21 @@ for i in range(training_steps):
 
 from matplotlib.pyplot import figure
 
-figure(figsize=(10,10))
+figure(figsize=(10, 10))
 # True Solution (found analitically)
 def true_solution(x):
-    return x**2 + 1
+    return x ** 2 + 1
+
 
 X = np.linspace(0, 1, 101)
 result = [g(x).numpy()[0, 0, 0] for x in X]
-#for i in X:
-  # result.append(f(i))
+# for i in X:
+# result.append(f(i))
 #  result.append(g(i).numpy()[0][0][0])
 
 S = true_solution(X)
-  
+
 plt.plot(X, S, label="Original Function")
 plt.plot(X, result, label="Neural Net Approximation")
-plt.legend(loc=2, prop={'size': 20})
+plt.legend(loc=2, prop={"size": 20})
 plt.show()
