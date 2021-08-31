8a1ed8452905cba65fee856c9dc1b7d602fa216a
Isaac Muñoz <isaacmues@gmail.com>
2021-08-31 12:03:33 -0500

    Add a second polynomial regression

---
+++ polynomial_regression_2.py
@@ -0,0 +1,29 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from tensorflow.keras import layers, initializers, optimizers
+from sklearn.preprocessing import PolynomialFeatures
+
+
+trX = np.linspace(0, 1, 51)
+trY = np.sin(trX * np.pi)
+
+n = 15
+trX_expanded = np.expand_dims(trX, axis=1)
+poly = PolynomialFeatures(n)
+trX_expanded = poly.fit_transform(trX_expanded)
+
+model = keras.Sequential(
+    [
+        layers.Dense(2, input_shape=(n+1,)),
+        layers.Dense(1, activation="sigmoid")
+    ]
+)
+
+model.compile(optimizer="adam", loss="mean_squared_error")
+model.fit(trX_expanded, trY, epochs=5000)
+
+plt.scatter(trX, trY)
+plt.plot(trX, model.predict(trX_expanded), color="red")
+plt.show()
