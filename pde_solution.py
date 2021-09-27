9a2f55822eb47328beee9337657118bc88e06d13
Isaac Muñoz <isaacmues@gmail.com>
2021-09-27 01:56:20 -0500

    The pde solution works now

--- pde_solution.py
+++ pde_solution.py
@@ -35,14 +35,24 @@ import matplotlib.pyplot as plt
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-def psi_a(x,y):
+def psi_a(z):
+
+    x = tf.reduce_sum(z * [[1.0, 0.0]], axis=1)
+    y = tf.reduce_sum(z * [[0.0, 1.0]], axis=1)
+    x = tf.expand_dims(x, axis=1)
+    y = tf.expand_dims(y, axis=1)
 
     return tf.exp(-x) * (x + y**3)
 
-def psi_t(x,y):
+
+def psi_t(z):
+
+    x = tf.reduce_sum(z * [[1.0, 0.0]], axis=1)
+    y = tf.reduce_sum(z * [[0.0, 1.0]], axis=1)
+    x = tf.expand_dims(x, axis=1)
+    y = tf.expand_dims(y, axis=1)
 
     e = np.exp(-1)
-
     f0 = y**3
     f1 = (y**3 + 1) * e
     g0 = x * tf.exp(-x)
@@ -53,38 +63,50 @@ def psi_t(x,y):
     A += (1 - y) * (g0 - x * e)
     A += y * (g1 - (1 - x + 2 * x * e))
 
-    return A + x * (1 - x) * y * (1 - y) * nn([x,y])
+    return A + x * (1 - x) * y * (1 - y) * nn(z)
 
-def laplacian(f, x, y, h):
+def laplacian(f, z, h):
 
-    d = f(x + h, y) + f(x, y + h)
-    d += f(x - h, y) + f(x, y - h)
-    d -= 4 * f(x, y)
+    d = f(z + [[h, 0.0]]) + f(z + [[0.0, h]]) 
+    d += f(z - [[h, 0.0]]) + f(z - [[0.0, h]]) 
+    d -= 4.0 * f(z)
     return d / h**2
 
 def lhs(x,y):
 
     return tf.exp(-x) * (x - 2.0 + y**3 + 6.0 * y)
 
-def custom_loss(p, y_pred):
+def custom_loss(z, y_pred):
 
-    x,y = p
-    error = laplacian(psi_t, x, y, 1e-3) - lhs(x,y)
-    return tf.reduce_sum(tf.square(error))
+    x = tf.reduce_sum(z * [[1.0, 0.0]], axis=1)
+    y = tf.reduce_sum(z * [[0.0, 1.0]], axis=1)
+    x = tf.expand_dims(x, axis=1)
+    y = tf.expand_dims(y, axis=1)
 
-input_x = Input(shape=[1])
-input_y = Input(shape=[1])
-p = Concatenate(axis=1)([input_x, input_y])
-p = Dense(16, activation="tanh")(p)
-p = Dense(8, activation="tanh")(p)
-p = Dense(8, activation="tanh")(p)
-output = Dense(1, activation="selu")(p)
-nn = Model(inputs=[input_x, input_y], outputs=output)
+    error = tf.reduce_sum(tf.square(laplacian(psi_t, z, 1e-3) - lhs(x,y)))
 
-points = tf.linspace(0.0, 1.0, 10)
+    return error
+
+inputs = Input(shape=[2])
+p = Dense(8, activation="tanh")(inputs)
+p = Dense(8, activation="tanh")(inputs)
+p = Dense(8, activation="tanh")(inputs)
+outputs = Dense(1, activation="selu")(p)
+nn = Model(inputs=inputs, outputs=outputs)
+nn.compile(optimizer="adam", loss=custom_loss)
+
+points = np.linspace(0.0, 1.0, 40)
 z = [[x,y] for x in points for y in points]
-x,y = tf.transpose(z)
-x = tf.expand_dims(x, axis=1)
-y = tf.expand_dims(y, axis=1)
+z = tf.constant(z, dtype=tf.float32)
 
-print(custom_loss([x,y], 0))
+nn.fit(z, z, epochs=200)
+
+x = tf.reshape(z[:,0], [40,40])
+y = tf.reshape(z[:,1], [40,40])
+psi = tf.reshape(psi_t(z), [40, 40])
+psi_2 = tf.reshape(psi_a(z), [40, 40])
+
+fig, ax = plt.subplots()
+CS = ax.contour(x, y, psi)
+CS = ax.contour(x, y, psi_2)
+plt.show()
